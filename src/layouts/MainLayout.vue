d13a98539325eb1913321d595fad890acb962da9
malo2b <malo.girard@gmail.com>
2021-12-16 23:14:32 +0100

    Update UI, ajout selection keyframe, previsualisation sur carousel

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Analyseur Video
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" :width="500" bordered>
+      <div v-for='image in images' :key="image.id">
+        <div class="keyframe-image col-4">
+          <q-img :src="image.src" @click="image.checked = !image.checked" /> <!-- , $emit('update:selected-image', image) -->
+          <div>Checked : {{ image.checked ? 'Oui' : 'Non'}}</div>
+          <br>
+        </div>
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view :images="images" />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<style>
+  .keyframe-image {
+    margin: 5px
+  }
+</style>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  },
+  data () {
+    return {
+      images: [
+        {
+          id: 1,
+          src: '/keyframes/149.jpg',
+          checked: true
+        },
+        {
+          id: 2,
+          src: '/keyframes/281.jpg',
+          checked: true
+        },
+        {
+          id: 3,
+          src: '/keyframes/331.jpg',
+          checked: true
+        },
+        {
+          id: 4,
+          src: '/keyframes/389.jpg',
+          checked: true
+        },
+        {
+          id: 5,
+          src: '/keyframes/627.jpg',
+          checked: true
+        },
+        {
+          id: 6,
+          src: '/keyframes/1011.jpg',
+          checked: true
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="carousel">
+      <q-carousel
+        animated
+        v-model="slide"
+        arrows
+        navigation
+        infinite
+      >
+        <q-carousel-slide v-for="image in imagesSelection" v-bind:key="image.id" :name="image.id" :img-src="image.src" />
+      </q-carousel>
+    </div>
+  </q-page>
+
+  <div class="q-pa-md">
+    <q-video
+      :ratio="16/9"
+      src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
+    />
+  </div>
+
+</template>
+
+<style>
+  .carousel {
+    width: 80%;
+    height: 100%;
+  }
+</style>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  props: ['images'],
+  setup () {
+    return {
+      slide: ref(1)
+    }
+  },
+  data () {
+  },
+  updated () {
+    console.log(this.images)
+  },
+  computed: {
+    imagesSelection () {
+      return this.images.filter(image => image.checked)
+    }
+  }
+})
+</script>
