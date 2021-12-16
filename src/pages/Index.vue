88e28df6c0c6d79335f9ceb22ec15de147854125
franck <contact@franckcolonna.fr>
2021-12-16 22:07:06 +0100

    ziiiing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,27 @@
 <template>
-  <modal-upload/>
+  <q-page class="flex flex-center bg-secondary">
+
+    <q-uploader
+        url="http://localhost:5000/upload"
+        color="primary"
+        flat
+        @upload="postUpload"
+        bordered
+        style="max-width: 300px"
+      />
+
+  </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import ModalUpload from '../components/ModalUpload.vue'
+
 export default defineComponent({
   name: 'PageIndex',
-  components: {
-    ModalUpload
+  methods: {
+    postUpload (event) {
+      console.log(event) // event is just the file name! The full response would be more useful
+    }
   }
 })
 </script>
