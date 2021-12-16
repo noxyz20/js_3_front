c547dbda32bbb7dc5cfb9e3982f5b414c607a877
malo2b <malo.girard@gmail.com>
2021-12-16 23:32:09 +0100

    avant implementation player video

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,13 +13,6 @@
     </div>
   </q-page>
 
-  <div class="q-pa-md">
-    <q-video
-      :ratio="16/9"
-      src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
-    />
-  </div>
-
 </template>
 
 <style>
